9c62db5e7cf2a114e166d0b9bbccfac1a047ed3d
MaximePetit03 <Maxime.petit@coda-student.school>
2025-10-02 13:35:16 +0200

    Exercice 2 terminé

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ print("Bonjour", prénom," ! ")
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "2":
-        exercice2()
+    if choix == "1":
+        exercice1()
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
